8f8797a65b41aea6e34eb7f769f474f9d8dea651
kojikojiprg <kojikoji.prg@gmail.com>
2020-06-04 22:42:23 +0900

    modify

--- neural_net/trainer.py
+++ neural_net/trainer.py
@@ -7,16 +7,16 @@ sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポ�
 
 
 class Trainer:
-    def __init__(self, network, x_train, t_train, x_test, t_test,
-                 epochs=20, mini_batch_size=100,
-                 optimizer='adam', optimizer_param={'lr': 0.01},
-                 evaluate_sample_num_per_epoch=None, verbose=True):
+    def __init__(
+        self, network,
+        epochs=20, mini_batch_size=100,
+        optimizer='adam', optimizer_param={'lr': 0.01},
+        evaluate_sample_num_per_epoch=None, verbose=True
+    ):
         self.network = network
+        self.x_train = None         # init in train function
+        self.t_train = None         # init in train function
         self.verbose = verbose
-        self.x_train = x_train
-        self.t_train = t_train
-        self.x_test = x_test
-        self.t_test = t_test
         self.epochs = epochs
         self.batch_size = mini_batch_size
         self.evaluate_sample_num_per_epoch = evaluate_sample_num_per_epoch
@@ -31,9 +31,9 @@ class Trainer:
             'adam': Adam}
         self.optimizer = optimizer_class_dict[optimizer.lower()](** optimizer_param)
 
-        self.train_size = x_train.shape[0]
-        self.iter_per_epoch = max(self.train_size / mini_batch_size, 1)
-        self.max_iter = int(epochs * self.iter_per_epoch)
+        self.train_size = 0         # init in train function
+        self.iter_per_epoch = 0     # init in train function
+        self.max_iter = 0           # init in train function
         self.current_iter = 0
         self.current_epoch = 0
 
@@ -58,28 +58,31 @@ class Trainer:
             self.current_epoch += 1
 
             x_train_sample, t_train_sample = self.x_train, self.t_train
-            x_test_sample, t_test_sample = self.x_test, self.t_test
 
             if self.evaluate_sample_num_per_epoch is not None:
                 t = self.evaluate_sample_num_per_epoch
                 x_train_sample, t_train_sample = self.x_train[:t], self.t_train[:t]
-                x_test_sample, t_test_sample = self.x_test[:t], self.t_test[:t]
 
             train_acc = self.network.accuracy(x_train_sample, t_train_sample)
-            test_acc = self.network.accuracy(x_test_sample, t_test_sample)
             self.train_acc_list.append(train_acc)
-            self.test_acc_list.append(test_acc)
 
             if self.verbose:
-                print("epoch:{}, train acc:{}, test acc:{}".format(self.current_epoch, train_acc, test_acc))
+                print("epoch:{}, train acc:{}".format(self.current_epoch, train_acc))
+
         self.current_iter += 1
 
-    def train(self):
+    def train(self, x_train, t_train, epochs=100, mini_batch_size=10):
+        self.x_train = x_train
+        self.t_train = t_train
+        self.train_size = x_train.shape[0]
+        self.iter_per_epoch = max(self.train_size / mini_batch_size, 1)
+        self.max_iter = int(epochs * self.iter_per_epoch)
+
         for i in range(self.max_iter):
             self.train_step()
 
-        test_acc = self.network.accuracy(self.x_test, self.t_test)
+    def test(self, x_test, t_test):
+        test_acc = self.network.accuracy(x_test, t_test)
 
         if self.verbose:
-            print("=============== Final Test Accuracy ===============")
             print("test acc:" + str(test_acc))
