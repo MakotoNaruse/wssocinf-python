443bd8c94cb9f3c710f415d983a995631fd96e72
kojikojiprg <kojikoji.prg@gmail.com>
2020-06-08 15:55:33 +0900

    first commit

---
+++ image_score.py
@@ -0,0 +1,22 @@
+from deep_convnet import DeepConvNet
+from util import img_to_matrix
+
+
+class ImageScore():
+    def __init__(self):
+        self.net = DeepConvNet()
+        self.net.load_params('./neural_net/network_params.pkl')
+
+    def _predict(self, file_path):
+        x = img_to_matrix(file_path)
+        return self.net.predict(x)
+
+    def score(self, file_path):
+        x = self._predict(file_path)
+
+        # 初期値(最低点)
+        score = 50
+        for i, xi in enumerate(x):
+            score += xi * (i + 1) * (50 / 5)
+
+        return score
